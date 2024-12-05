dd9eeab5fffc33b4b48ef60365170e468e2c9a28
Jeffy Jefferson <jeffyj080@gmail.com>
2024-12-05 16:35:30 +0200

    Comments

--- Tokenizer.h
+++ Tokenizer.h
@@ -20,11 +20,13 @@ public:
 		string token, tempStr;
 		stringstream strToken(Input);
 
-		// 
+		// Decleration of variables
 		int i = 0, k = 0;
 
+		// Gets the string to tokenize
 		getline(strToken, token);
 
+		// Loops through the string
 		for (i = 0; i < token.length(); i++)
 		{
 			//if (token[i] == ',')
@@ -55,6 +57,7 @@ public:
 			//	k = i + 1;
 			//}
 
+			// Checks for whitespace as deliminator
 			if (token[i] == ' ')
 			{
 				tempStr = token.substr(k, i - k);
@@ -63,7 +66,7 @@ public:
 					Tokens.push_back(tempStr);
 				}
 				k = i + 1;
-			}
+			}// If
 			else if (i == token.length() - 1)
 			{
 				tempStr = token.substr(k, i - k + 1);
@@ -71,24 +74,29 @@ public:
 				{
 					Tokens.push_back(tempStr);
 				}
-			}
-		}
+			}// Else if
+		}// For
 
+		// Return
 		return Tokens;
-	}
+	}// tokenize
 
 private:
+	// Checks for whitespace
 	static bool isWhitespace(string c)
 	{
+		// Checks for whitespace
 		if (c == " " || c == "\t" || c == "\n" || c == "\r" || c == "")
 		{
+			// Return
 			return true;
-		}
+		}// If
 		else
 		{
+			// Return
 			return false;
-		}
-	}
+		}// Else
+	}// isWhitespace
 };
 
 #endif // TOKNIZER_H
